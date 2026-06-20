8cf0e8ee85b3f6abf63eecd59136b50d1ee32ac4
wheon06 <up2kid@gmail.com>
2026-06-20 20:09:35 +0900

    fix: refreshToken 유효성 검사를 TokenRefreshRequest 생성자에 추가했습니다.

--- src/main/java/com/team/moodit/api/controller/v1/request/TokenRefreshRequest.java
+++ src/main/java/com/team/moodit/api/controller/v1/request/TokenRefreshRequest.java
@@ -1,6 +1,14 @@
 package com.team.moodit.api.controller.v1.request;
 
+import com.team.moodit.support.error.ApiException;
+import com.team.moodit.support.error.ErrorType;
+
 public record TokenRefreshRequest(
         String refreshToken
 ) {
+    public TokenRefreshRequest {
+        if (refreshToken == null || refreshToken.isBlank()) {
+            throw new ApiException(ErrorType.INVALID_REQUEST);
+        }
+    }
 }
