c5678fc03959e0d1ab38764bd1820a40a0ed302e
이호준 <jake525@naver.com>
2026-06-22 15:07:28 +0900

    feat : MatchController 추가 및 작성

---
+++ src/main/java/com/team/moodit/api/controller/v1/MatchController.java
@@ -0,0 +1,26 @@
+package com.team.moodit.api.controller.v1;
+
+import com.team.moodit.api.controller.v1.request.MatchCreateRequest;
+import com.team.moodit.api.controller.v1.response.MatchCreateResponse;
+import com.team.moodit.domain.match.MatchService;
+import com.team.moodit.support.auth.ApiUser;
+import com.team.moodit.support.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MatchController {
+    private final MatchService matchService;
+
+    @PostMapping("/v1/matches")
+    public ApiResponse<MatchCreateResponse> createMatch(
+            ApiUser apiUser,
+            @RequestBody MatchCreateRequest matchCreateRequest
+            ){
+
+
+    }
+}
