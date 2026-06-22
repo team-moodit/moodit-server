ca76924ba373303c6fc6f8b82360b689ec63b84a
이호준 <jake525@naver.com>
2026-06-22 10:52:50 +0900

    feat: MatchEntity 설계 수정 및 MatchStatus Enum 추가
    - 변경 사항:
      1. domain/enums 에 `MatchStatus` 생성
      2. `MatchEntity` status 필드 타입을 String에서 Enum으로 변경
      3. `MatchEntity` 기본 엔티티(`BaseIdentity`) 상속 구조로 변경
      4. `MatchRepository` 추가

---
+++ src/main/java/com/team/moodit/domain/enums/MatchStatus.java
@@ -0,0 +1,7 @@
+package com.team.moodit.domain.enums;
+
+public enum MatchStatus {
+    ING,
+    TEMP,
+    DONE;
+}
---
+++ src/main/java/com/team/moodit/storage/db/core/MatchEntity.java
@@ -0,0 +1,45 @@
+package com.team.moodit.storage.db.core;
+
+import com.team.moodit.domain.enums.MatchStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "match")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MatchEntity extends BaseIdEntity {
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "title", nullable = false, length = 20)
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 20)
+    private MatchStatus status;
+
+
+    @Column(name = "initial_image_count", nullable = false)
+    private Long initialImageCount;
+
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+
+
+}
