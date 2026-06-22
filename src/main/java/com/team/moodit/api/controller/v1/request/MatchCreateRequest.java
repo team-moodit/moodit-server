9f35241254f6669f5dd1006a37194810d6542e05
이호준 <jake525@naver.com>
2026-06-22 17:38:42 +0900

    refactor : ErroType 추가 및 MatchCreateRequest 검증 로직 추가

--- src/main/java/com/team/moodit/api/controller/v1/request/MatchCreateRequest.java
+++ src/main/java/com/team/moodit/api/controller/v1/request/MatchCreateRequest.java
@@ -1,12 +1,26 @@
 package com.team.moodit.api.controller.v1.request;
 
+import com.team.moodit.support.error.ApiException;
+import com.team.moodit.support.error.ErrorType;
+
 import java.util.List;
 
 public record MatchCreateRequest(
    String title,
    List<Long> images
 ) {
-    if(title == null || title.isBlank()){
+    public MatchCreateRequest {
+        // 제목 검증
+        if (title == null || title.isBlank()) {
+            throw new ApiException(ErrorType.INVALID_TITLE_FAILED);
+        }
+
+        //이미지 개수 검증 (요구사항: 8장~32장)
+        if (images == null || images.size() < 8 || images.size() > 32) {
+            throw new ApiException(ErrorType.INVALID_IMAGE_COUNT_FAILED);
+        }
 
     }
+
 }
+
