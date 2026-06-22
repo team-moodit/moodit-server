6bd90f8547eeaf7beab4441f58e5b2b364c4a9ee
이희연 HeeYeon Lee <dgc394722@gmail.com>
2026-06-22 16:03:24 +0900

    feat: ErrorType에 NOT_FOUND_MATCH_RESULT 타입 추가

--- src/main/java/com/team/moodit/support/error/ErrorType.java
+++ src/main/java/com/team/moodit/support/error/ErrorType.java
@@ -16,6 +16,9 @@ public enum ErrorType {
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.", LogLevel.INFO),
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰 입니다.", LogLevel.INFO),
 
+    // Match Result
+    NOT_FOUND_MATCH_RESULT(HttpStatus.NOT_FOUND, "매치 결과를 찾을 수 없습니다.", LogLevel.INFO),
+
     // File
     FILE_UPLOADING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드 중 알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요.", LogLevel.ERROR);
 
