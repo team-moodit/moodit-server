d5462b9462980a08d378c5880780394a0234147c
이호준 <jake525@naver.com>
2026-06-23 19:16:41 +0900

    refactor : 도메인 Match 객체 생성자 제거 및 일부 컬럼 제거 완료

--- src/main/java/com/team/moodit/domain/match/Match.java
+++ src/main/java/com/team/moodit/domain/match/Match.java
@@ -7,17 +7,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class Match {
-    private Long id;
     private Long userId;
     private String title;
-    private MatchState state;
-    private Integer initialImageCount;
 
-    public Match(Long userId, String title, MatchState state, Integer initialImageCount) {
-        this.userId = userId;
-        this.title = title;
-        this.state = state;
-        this.initialImageCount = initialImageCount;
-    }
+
 
 }
