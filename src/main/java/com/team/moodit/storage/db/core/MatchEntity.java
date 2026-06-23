9ed0e587f2b7978d1fba15de2c51affce35af6bf
이희연 HeeYeon Lee <up2kid@gmail.com>
2026-06-23 20:16:46 +0900

    refactor: MatchEntity에서 import 구문 정리 및 어노테이션 명시적 추가
    - 불필요한 import 제거 및 lombok 관련 명시적 수준 설정(@Getter 등)
    - @Column, @Enumerated 등 JPA 어노테이션 명시적으로 추가
    - 기존 import 정리로 가독성 및 코드 명확성 향상

--- src/main/java/com/team/moodit/storage/db/core/MatchEntity.java
+++ src/main/java/com/team/moodit/storage/db/core/MatchEntity.java
@@ -1,9 +1,15 @@
 package com.team.moodit.storage.db.core;
 
 import com.team.moodit.domain.enums.MatchState;
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
